1008aaec567b9622cc712c71231e8970bc537936
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2016-02-22 16:52:28 +0000

    add second script

---
+++ scripts/gpio_part_2.py
@@ -0,0 +1,45 @@
+from flask import Flask, send_from_directory
+import RPi.GPIO as GPIO
+from time import time
+
+GPIO.setmode(GPIO.BOARD)
+
+pin = 36
+
+GPIO.setup(pin, GPIO.OUT)
+
+app = Flask(__name__)
+
+
+# Return hello world to the index page
+@app.route('/')
+def hello_world():
+    return '<h1>Greetings, Stevenage Branch!</h1>' \
+           '<p>We hope you enjoy the show!</p>'
+
+
+@app.route('/lamp/on')
+def turn_me_on():
+    GPIO.output(pin, GPIO.HIGH)
+    return "<h1>Turning the lamp on</h1>"
+
+
+@app.route('/lamp/off')
+def turn_me_off():
+    GPIO.output(pin, GPIO.LOW)
+    return "<h1>Turning the lamp off</h1>"
+
+
+# Example of how to serve a static file
+@app.route('/static/<path:filename>')
+def serve_static_file(filename):
+    return send_from_directory('static', filename)
+
+
+if __name__ == '__main__':
+    try:
+        app.run(debug=True)
+        # app.run(host='0.0.0.0', port=8080, debug=True)
+    finally:
+        GPIO.cleanup()
+        print "Bye"
