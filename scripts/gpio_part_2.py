513b8626f88ed44b8ed02487ec46377fe15c458a
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2016-02-23 12:19:17 +0000

    Add a mock gpio so we can see the server response when we don't run things on the rip

--- scripts/gpio_part_2.py
+++ scripts/gpio_part_2.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory
-import RPi.GPIO as GPIO
-from time import time
+try:
+   import RPi.GPIO as GPIO
+except:
+    from MockGPIO import MockGPIO
+    GPIO = MockGPIO()
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -42,4 +45,4 @@ if __name__ == '__main__':
         # app.run(host='0.0.0.0', port=8080, debug=True)
     finally:
         GPIO.cleanup()
-        print "Bye"
+        print "Bye."
