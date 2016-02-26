91dd6ca1c52426af414ca720e619315a8883b9c4
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2016-02-26 23:50:02 +0000

    Improve part 2 appearance.

--- scripts/gpio_part_2.py
+++ scripts/gpio_part_2.py
@@ -17,20 +17,20 @@ app = Flask(__name__)
 # Return hello world to the index page
 @app.route('/')
 def hello_world():
-    return '<h1>Greetings, Stevenage Branch!</h1>' \
-           '<p>We hope you enjoy the show!</p>'
+    return '<h1 align="center">Greetings, Stevenage Branch!</h1>' \
+           '<p align="center">We hope you enjoy the show!</p>'
 
 
 @app.route('/lamp/on')
 def turn_me_on():
     GPIO.output(pin, GPIO.HIGH)
-    return "<h1>Turning the lamp on</h1>"
+    return '<h1 align="center">Turning the lamp on</h1>'
 
 
 @app.route('/lamp/off')
 def turn_me_off():
     GPIO.output(pin, GPIO.LOW)
-    return "<h1>Turning the lamp off</h1>"
+    return '<h1 align="center">Turning the lamp off</h1>'
 
 
 # Example of how to serve a static file
