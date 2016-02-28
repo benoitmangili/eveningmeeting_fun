ba03e5d6301e6a5c6477b80487c16a064300f791
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2016-02-28 10:38:34 +0000

    add controller to server script

--- scripts/gpio_part_4.py
+++ scripts/gpio_part_4.py
@@ -3,7 +3,7 @@ from time import sleep
 # Can only use DS18B20 on the raspberry pi
 #from sensor.sensor import DS18B20 as sensor
 from sensor.DummySensor import DummySensor as sensor
-
+from Controller import Controller
 try:
    import RPi.GPIO as GPIO
 except:
@@ -18,6 +18,11 @@ GPIO.setup(pin, GPIO.OUT)
 
 # Instantiate Sensor Class
 sensor = sensor()
+
+# Instantiate Controller Class
+t_lower = 15
+t_upper = 20
+controller = Controller(t_lower, t_upper)
 
 app = Flask(__name__)
 
@@ -40,12 +45,12 @@ def set_set_point():
     global threshold
     if request.method == 'POST':
         data = request.form
-        threshold = data['set_point']
+        set_point_upper = data['set_point_upper']
+        set_point_lower = data['set_point_lower']
+        controller.update_temperature_range(set_point_lower, set_point_upper)
     else:
         return jsonify(threshold=threshold)
 
-# add method that decides whether to turn the heater on or off
-# (don't call /lamp, wrap code to change pin state in function and reuse here.)
 
 @app.route('/lamp', methods=['GET', 'PUT'])
 def lamp_stuff():
@@ -86,6 +91,13 @@ def get_gpio_state(pin):
 
     # Return current state
     return state
+
+
+def get_heater_command():
+    # Returns true if the heater is to be turned on or false otherwise
+    measurement = sensor.getMeasurement()
+    command = controller.get_command(measurement.value)
+    return command
 
 if __name__ == '__main__':
     try:
