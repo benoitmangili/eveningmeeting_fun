0f4e4347b322b0aafe16bab35e7946f17dac3a56
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2016-02-28 23:08:57 +0000

    Add the time stamp and the tweet vote to the tweet list

--- scripts/gpio_part_5.py
+++ scripts/gpio_part_5.py
@@ -33,18 +33,17 @@ def hello_world():
 def get_last_tweet(num_tweets):
     # Limit the number of tweets that people can get.
     tweets = twitter.get_tweets('tweet_pi_tweet', num_tweets)
+    tweet_list = []
     for tweet in tweets:
+        tweet_stamp = tweet.created_at
+        tweet_list.append(json.dumps({
+            "author": tweet.author.screen_name,
+            "tweet": tweet.text,
+            "timeStamp": str(tweet_stamp),
+            "vote": check_tweet_vote(tweet.text)}))
+    return jsonify(tweet_list=tweet_list)
 
-
-    tweet_text = [tweet.text for tweet in tweets]
-    tweet_author = [tweet.author.screen_name for tweet in tweets]
-
-    # TODO: get time stamp of tweet
-    # TODO: Set vote based on tweet containing colder/hotter vote =[] if not a valid tweet.
-    return json.dumps([{"author": name, "tweet": text} for name, text in zip(tweet_author, tweet_text)])
-
-# TODO: function to return is there is a new tweet
-
+#TODO: function to return is there is a new tweet
 #TODO: This should not be an endpoint and and run all the time. Should be put in a loop. Put this in a separate thread.
 # Use the periodic timer
 @app.route('/twitter_vote')
@@ -59,8 +58,8 @@ def twitter_vote():
 
     # Turn on heater via voting system
     # change to hotter/colder. 'make it hot, make it hotter'
-    num_people_too_hot = sum([tweet.find('hot')>0 for tweet in tweet_text])
-    num_people_too_cold = sum([tweet.find('cold')>0 for tweet in tweet_text])
+    num_people_too_hot = sum([tweet.find('hotter')>0 for tweet in tweet_text])
+    num_people_too_cold = sum([tweet.find('colder')>0 for tweet in tweet_text])
 
     turn_on_heater = num_people_too_cold > num_people_too_hot
 
@@ -88,6 +87,15 @@ def emit_new_tweet(tweet):
         time.sleep(0.5)
         print 'tweet ?', tweet
         socketio.emit('new_tweet', str(i) , broadcast=True)
+
+
+def check_tweet_vote(tweet_text):
+    if tweet_text.find('hotter') > 0:
+        return True
+    elif tweet_text.find('colder') > 0:
+        return False
+    else:
+        return []
 
 if __name__ == '__main__':
     try:
